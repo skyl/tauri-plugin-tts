40d632b737499fa7258e0567e6109db1f8906bc7
Skylar Saveland <skylar.saveland@gmail.com>
2025-05-22 23:09:23 -0400

    revert unnecessary changes

--- src/mobile.rs
+++ src/mobile.rs
@@ -8,13 +8,6 @@ use tauri::{
 #[cfg(target_os = "ios")]
 tauri::ios_plugin_binding!(init_plugin_tts);
 
-// This struct must match your mobile plugin argument structure!
-#[derive(serde::Serialize)]
-pub struct SpeakArgs {
-    pub text: String,
-    pub language: Option<String>,
-}
-
 pub fn init<R: Runtime, C: DeserializeOwned>(
     _app: &AppHandle<R>,
     api: PluginApi<R, C>,
@@ -32,10 +25,6 @@ pub struct Tts<R: Runtime>(PluginHandle<R>);
 
 impl<R: Runtime> Tts<R> {
     pub fn speak(&self, text: String, language: Option<String>) -> crate::Result<()> {
-        println!(
-            "Starting speak operation with text: {}, language: {:?}",
-            text, language
-        );
         let args = SpeakArgs { text, language };
         self.0.run_mobile_plugin("speak", Some(args)).map_err(|e| {
             println!("Speech error: {:?}", e); // Debug log
